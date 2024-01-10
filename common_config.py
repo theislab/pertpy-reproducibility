afbe106bed3a0ee8755ae32a966a9947ffc2b80c
zethson <lukas.heumos@posteo.net>
2024-01-10 23:39:47 +0100

    Add structure
    Signed-off-by: zethson <lukas.heumos@posteo.net>

---
+++ common_config.py
@@ -0,0 +1,36 @@
+import scanpy as sc
+import matplotlib.pyplot as plt
+
+plt.rcParams["font.family"] = "sans-serif"
+plt.rcParams["font.sans-serif"] = ["Arial", "Helvetica"]
+
+
+def figure_journal_basic():
+    sc.set_figure_params(
+        dpi=300,
+        dpi_save=300,
+        figsize=(4, 4),
+        frameon=False,
+        facecolor=None,
+        transparent=True,
+        vector_friendly=True,
+    )
+
+
+def figure_jupyter():
+    sc.set_figure_params(
+        dpi=80,
+        dpi_save=150,
+        figsize=(8, 8),
+        frameon=False,
+        facecolor="white",
+        transparent=False,
+        vector_friendly=True,
+    )
+
+
+mhv68_pcls_ct_color_map = {}
+
+mcfarland_ct_color_map = {}
+
+repogle_ct_color_map = {}
