05bceaae294df43390f1e0bf60815435deaada04
stefanpeidli <stefanpeidli@gmail.com>
2025-02-21 13:27:56 +0100

    Restructure benchmark for snakemake

--- benchmark/scripts/augur.r
+++ benchmark/scripts/augur.r
@@ -5,16 +5,21 @@ library(magrittr)
 library(ggplot2)
 library(viridis)
 
+# I/O
+Bhattacherjee2019.rds <- "/home/icb/zihe.zheng/pertpy_benchmark/data/Bhattacherjee2019.rds"
+out_cocaine <- "../results/augur_cocaine.csv"
+out_sim <- "../results/augur_sim.csv"
+
 # load data
 data("sc_sim")
 
 # predict with select variance feature selection
 augur_v = calculate_auc(sc_sim, classifier = 'rf', subsample_size = 20, n_threads = 16, select_var = T)
-augur_v$AUC
+write.csv(augur_v$AUC, out_sim)
 
 # differntial prioritization
 # load data
-sc = readRDS("/home/icb/zihe.zheng/pertpy_benchmark/data/Bhattacherjee2019.rds")
+sc = readRDS(Bhattacherjee2019.rds)
 Idents(sc) = sc@meta.data$label
 sc_sub1 = subset(sc, idents = c("Maintenance_Cocaine", "withdraw_15d_Cocaine"))
 sc_sub2 = subset(sc, idents = c("Maintenance_Cocaine", "withdraw_48h_Cocaine"))
@@ -35,3 +40,5 @@ pvals = calculate_differential_prioritization(augur1 = bhattacherjee_results_15,
                                               augur2 = bhattacherjee_results_48,
                                               permuted1 = bhattacherjee_results_15_permute,
                                               permuted2 = bhattacherjee_results_48_permute)
+
+write.csv(pvals, out_cocaine)
