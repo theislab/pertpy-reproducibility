1daf25a7e6b433c9976021284ef53a56cfbc0f19
Lukas Heumos <lukas.heumos@posteo.net>
2025-02-21 15:14:49 +0100

    Add CINEMA-OT
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

---
+++ benchmark/cinema_ot_new.py
@@ -0,0 +1,42 @@
+import scanpy as sc
+import cinemaot as co
+import pertpy as pt
+import numpy as np
+import time
+
+adata = pt.dt.cinemaot_example()
+sc.pp.sample(adata, n=10000, replace=True)
+adata.X = adata.raw.X.copy()
+
+sc.pp.pca(adata)
+
+cot = pt.tl.Cinemaot()
+# warm up cache & jit
+de = cot.causaleffect(
+    adata,
+    pert_key="perturbation",
+    control="No stimulation",
+    return_matching=True,
+    thres=1,
+    smoothness=3e-5,
+    eps=1e-3,
+    solver="Sinkhorn",
+    preweight_label="cell_type0528",
+)
+
+start = time.time()
+
+de = cot.causaleffect(
+    adata,
+    pert_key="perturbation",
+    control="No stimulation",
+    return_matching=True,
+    thres=1,
+    smoothness=3e-5,
+    eps=1e-3,
+    solver="Sinkhorn",
+    preweight_label="cell_type0528",
+)
+
+runtime = time.time() - start
+print(f"Runtime: {runtime:.2f} seconds")
