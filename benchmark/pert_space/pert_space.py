a6d57b2b8bd94df6ffd0ec6998a81baf14792ef3
Lukas Heumos <lukas.heumos@posteo.net>
2025-02-22 14:31:19 +0100

    Add pert space
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

---
+++ benchmark/pert_space/pert_space.py
@@ -0,0 +1,143 @@
+import os
+import time
+import warnings
+
+import pertpy as pt
+
+warnings.filterwarnings("ignore")
+os.environ["KMP_WARNINGS"] = "off"
+
+adata = pt.dt.norman_2019()
+
+G1_CYCLE = [
+    "CDKN1A",
+    {"CDKN1B", "CDKN1A"},
+    "CDKN1B",
+    {"CDKN1C", "CDKN1A"},
+    {"CDKN1C", "CDKN1B"},
+    "CDKN1C",
+]
+
+ERYTHROID = [
+    {"CBL", "CNN1"},
+    {"CBL", "PTPN12"},
+    {"CBL", "PTPN9"},
+    {"CBL", "UBASH3B"},
+    {"SAMD1", "PTPN12"},
+    {"SAMD1", "UBASH3B"},
+    {"UBASH3B", "CNN1"},
+    {"UBASH3B", "PTPN12"},
+    {"UBASH3B", "PTPN9"},
+    {"UBASH3B", "UBASH3A"},
+    {"UBASH3B", "ZBTB25"},
+    {"BPGM", "SAMD1"},
+    "PTPN1",
+    {"PTPN12", "PTPN9"},
+    {"PTPN12", "UBASH3A"},
+    {"PTPN12", "ZBTB25"},
+    {"UBASH3A", "CNN1"},
+]
+
+PIONEER_FACTORS = [
+    {"FOXA1", "FOXF1"},
+    {"FOXA1", "FOXL2"},
+    {"FOXA1", "HOXB9"},
+    {"FOXA3", "FOXA1"},
+    {"FOXA3", "FOXF1"},
+    {"FOXA3", "FOXL2"},
+    {"FOXA3", "HOXB9"},
+    "FOXA3",
+    {"FOXF1", "FOXL2"},
+    {"FOXF1", "HOXB9"},
+    {"FOXL2", "MEIS1"},
+    "HOXA13",
+    "HOXC13",
+    {"POU3F2", "FOXL2"},
+    "TP73",
+    "MIDN",
+    {"LYL1", "IER5L"},
+    "HOXC13",
+    {"DUSP9", "SNAI1"},
+    {"ZBTB10", "SNAI1"},
+]
+
+GRANULOCYTE_APOPTOSIS = [
+    "SPI1",
+    "CEBPA",
+    {"CEBPB", "CEBPA"},
+    "CEBPB",
+    {"CEBPE", "CEBPA"},
+    {"CEBPE", "CEBPB"},
+    {"CEBPE", "RUNX1T1"},
+    {"CEBPE", "SPI1"},
+    "CEBPE",
+    {"ETS2", "CEBPE"},
+    {"KLF1", "CEBPA"},
+    {"FOSB", "CEBPB"},
+    {"FOSB", "CEBPE"},
+    {"ZC3HAV1", "CEBPA"},
+    {"JUN", "CEBPA"},
+]
+
+PRO_GROWTH = [
+    {"CEBPE", "KLF1"},
+    "KLF1",
+    {"KLF1", "BAK1"},
+    {"KLF1", "MAP2K6"},
+    {"KLF1", "TGFBR2"},
+    "ELMSAN1",
+    {"MAP2K3", "SLC38A2"},
+    {"MAP2K3", "ELMSAN1"},
+    "MAP2K3",
+    {"MAP2K3", "MAP2K6"},
+    {"MAP2K6", "ELMSAN1"},
+    "MAP2K6",
+    {"MAP2K6", "KLF1"},
+]
+
+MEGAKARYOCYTE = [
+    {"MAPK1", "TGFBR2"},
+    "MAPK1",
+    {"ETS2", "MAPK1"},
+    "ETS2",
+    {"CEBPB", "MAPK1"},
+]
+
+programmes = {
+    "G1 cell cycle": G1_CYCLE,
+    "Erythroid": ERYTHROID,
+    "Pioneer factors": PIONEER_FACTORS,
+    "Granulocyte apoptosis": GRANULOCYTE_APOPTOSIS,
+    "Pro-growth": PRO_GROWTH,
+    "Megakaryocyte": MEGAKARYOCYTE,
+}
+
+gene_programme = []
+
+for target_pert in adata.obs["perturbation_name"]:
+    if target_pert == "control":
+        gene_programme.append("Control")
+        continue
+
+    found_programme = False
+    for programme, pert_list in programmes.items():
+        for pert in pert_list:
+            if (isinstance(pert, set) and pert == set(target_pert.split("+"))) or (target_pert == pert):
+                gene_programme.append(programme)
+                found_programme = True
+                break
+
+    if not found_programme:
+        gene_programme.append("Unknown")
+
+adata.obs["gene_programme"] = gene_programme
+
+adata = adata[adata.obs["gene_programme"] != "Unknown"]
+
+start = time.time()
+
+ps = pt.tl.LRClassifierSpace()
+psadata = ps.compute(adata, embedding_key="X_pca", target_col="perturbation_name")
+
+runtime = time.time() - start
+print(f"Runtime: {runtime:.2f} seconds")
